ff42ae249a38f09b7cd22a948c2f5dc604b9581e
kingerious <mohammedsiddiqui2009@gmail.com>
2022-05-15 20:10:07 +0530

    should work

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist/js"),
     // filename: "main.js",
   },
-  watch: true,
+  watch: false,
   experiments: {
     topLevelAwait: true,
   },
