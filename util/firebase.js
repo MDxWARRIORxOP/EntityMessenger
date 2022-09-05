a3fada39954ca851be4fdd38a26492f9f14cf57e
MDxWARRIORxOP <mohammedsiddiqui2009@gmail.com>
2022-09-05 16:25:45 +0530

    SHOULD FINALLY WORK LMAO

--- util/firebase.js
+++ util/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import * as Config from "./config.json";
 
 const firebaseConfig = {
-  apiKey: Config.NEXT_PUBLIC_FIREBASE_PUBLIC_KEY,
-  authDomain: Config.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: Config.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: Config.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: Config.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: Config.NEXT_PUBLIC_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const App = initializeApp(firebaseConfig);
